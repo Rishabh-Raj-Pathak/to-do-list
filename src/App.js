31c3eae0881b5bc5f4d5d29c8634492c85be46e3
RishabhRajPathak <rishabhrajpathak06@gmail.com>
2024-01-15 12:22:22 +0530

    "Commit"

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+    const[newItem, setNewItem] = useState("")
+    const[todos, setTodos] = useState([])
+
+    function handleSubmit(e) {
+      e.preventDefault(); 
+
+      setTodos(currentTodos => {
+        return[
+          ...currentTodos,
+         {title:newItem,
+          id: crypto.randomUUID,
+          completed: false},
+        ];
+      });
+      
+      setNewItem("")
+    }
+
+    function toggleTodo(id, completed){
+      setTodos(currentTodos => {
+        return currentTodos.map(todo => {
+          if(todo.id === id){
+            return {...todo, completed}
+          }
+
+          return todo;
+        })
+      })
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='main'>
+      <label htmlFor=""><h2>New Item</h2></label>
+      <input  value={newItem}  
+      onChange={e => setNewItem(e.target.value)} type="text" 
+      id='item'
+      className='InputBox' /><br /><br />
+      <button onClick={handleSubmit} className='btn'>Add</button>
     </div>
+
+    <div className='list'>
+    <h2>Todo List</h2>
+    <ul>
+      {todos.map(todo => {
+        return (<li key={todo.id}>
+          <input type="checkbox" checked={todo.completed}
+            onChange={e => toggleTodo(todo.id, e.target.checked)}
+          />{todo.title}<button>Delete</button>
+        </li>)
+      })}
+    </ul>
+    </div>
+    </>
   );
 }
 
